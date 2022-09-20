17babed85f6c7a76f995d8303167425b6b02ff4a
sakthi-sakthi <sakthiganapathis97@gmail.com>
2022-09-20 18:26:26 +0530

    template

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Restaurant</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Restaurantly - v3.9.0
+  * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  @include('frontend.include.topbar')
+
+  <!-- ======= Header ======= -->
+  @include('frontend.include.header')
+  <!-- End Header -->
+  
+  <!-- ======= Hero Section ======= -->
+  @include('frontend.include.hero')
+ <!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    @include('frontend.include.about')
+    <!-- End About Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    @include('frontend.include.whyus')
+    <!-- End Why Us Section -->
+
+    <!-- ======= Menu Section ======= -->
+    @include('frontend.include.menu')
+    <!-- End Menu Section -->
+
+    <!-- ======= Specials Section ======= -->
+    @include('frontend.include.special')
+    <!-- End Specials Section -->
+
+    <!-- ======= Events Section ======= -->
+     @include('frontend.include.events')
+    <!-- End Events Section -->
+
+    <!-- ======= Book A Table Section ======= -->
+    @include('frontend.include.booktable')
+    <!-- End Book A Table Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    @include('frontend.include.testimonial')
+    <!-- End Testimonials Section -->
+
+    <!-- ======= Gallery Section ======= -->
+    @include('frontend.include.gallery')
+    <!-- End Gallery Section -->
+
+    <!-- ======= Chefs Section ======= -->
+    @include('frontend.include.chef')
+    <!-- End Chefs Section -->
+
+    <!-- ======= Contact Section ======= -->
+     @include('frontend.include.contact')
+   <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  @include('frontend.include.footer')
+  <!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
